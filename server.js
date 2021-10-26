185c471741f26f5eeee36ba094ef14805c893e7d
Socks <patricksutcliffe123@gmail.com>
2021-10-25 20:18:34 -0400

    setting it up still

--- server.js
+++ server.js
@@ -19,22 +19,22 @@ const sess = {
     store: new SequelizeStore({
       db: sequelize
     })
-  };
+  }
 
 app.use(session(sess))
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers })
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(require('./controllers/'));
+app.use(require('./controllers/'))
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
-});
+  console.log(`App listening on port ${PORT}!`)
+  sequelize.sync({ force: false })
+})
